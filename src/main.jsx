0f22027d534b5647e75fdbb08bf5829e68a371d3
root <root@srv546944.hstgr.cloud>
2025-10-20 12:08:10 +0200

    renomme Composant1

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
-import Composant1 from "./composant1.jsx";
+import Composant1 from "./Composant1.jsx";
 import Composant2 from "./Composant2.jsx";
 import { Provider } from "./Context.jsx";
 import Formulaire from "./Formulaire.jsx";
