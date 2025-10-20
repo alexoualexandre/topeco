4c1599aeaa290d72d3c3b3fd74a50dc47bc262a7
root <root@srv546944.hstgr.cloud>
2025-10-20 12:06:36 +0200

    delete composant1

--- src/composant1.jsx
+++
@@ -1,9 +0,0 @@
-import { MyContext } from "./Context.jsx";
-
-function Composant1() {
-  // const {elem,setElem} = MyContext();
-
-  return <h1 className="composant-1">Composant 1</h1>;
-}
-
-export default Composant1;
