71a3d9caa1617c99f62c6178f5e6793b5c06c3c2
alexandre michiels <alexoualexandre1@gmail.com>
2025-10-20 10:31:52 +0200

    clonage

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ export default function App() {
   return (
     <>
       <header>
-        <h1 style={{ color: "white" }}>headers0</h1>
+        <h1 style={{ color: "white" }}>headers00000</h1>
 
         <nav>
           <ul className="ul-nav">
